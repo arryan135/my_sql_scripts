973f1b73acadc4d9c63c230ee64ec10c5ad0c9c1
arryan <arryan@umich.edu>
2021-08-03 18:46:08 -0400

    addtional refinied selection methods

--- book_shop_db/refining-selections/like.sql
+++ book_shop_db/refining-selections/like.sql
@@ -1,5 +1,5 @@
 -- example %da% means anything da anything
--- SELECT title, author_fname FROM books WHERE author_fname LIKE "%da%";
+SELECT title, author_fname FROM books WHERE author_fname LIKE "%da%";
 
 -- Ending with da
 SELECT title, author_fname FROM books WHERE author_fname LIKE "%da";
@@ -15,3 +15,31 @@ SELECT title FROM books WHERE title LIKE "%\%%";
 
 --  search for book title with '_' character
 SELECT title FROM books WHERE title LIKE "%\_%";
+
+-- titles that contain the word stories
+SELECT title FROM books WHERE title LIKE "%stories%";
+
+-- longest book in the book db
+SELECT title, pages FROM books ORDER BY pages DESC LIMIT 1;
+
+-- summary of the most recent books
+SELECT 
+  CONCAT(title, " - ", released_year) AS "summary"
+  FROM books 
+  ORDER BY released_year DESC LIMIT 3;
+
+-- books where author last name contains space character
+SELECT title, author_lname FROM books WHERE author_lname LIKE "% %";
+
+-- 3 books with the lowest stock
+SELECT 
+  title, released_year, stock_quantity FROM books
+  ORDER BY stock_quantity, title LIMIT 3;
+
+SELECT title, author_lname FROM books ORDER BY author_lname, title;
+
+-- sort alphabetically by last name
+SELECT 
+  CONCAT(UPPER("my favorite author is ") , UPPER(author_fname), " ", UPPER(author_lname), "!") AS "yell"
+  FROM books
+  ORDER BY author_lname;
