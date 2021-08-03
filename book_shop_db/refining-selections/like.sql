e63e2743cd6bb9fd34f450ea4e49101c06850b26
arryan <arryan@umich.edu>
2021-08-03 18:27:46 -0400

    add extra use cases for LIKE

--- book_shop_db/refining-selections/like.sql
+++ book_shop_db/refining-selections/like.sql
@@ -1,8 +1,17 @@
 -- example %da% means anything da anything
-SELECT title, author_fname FROM books WHERE author_fname LIKE "%da%";
+-- SELECT title, author_fname FROM books WHERE author_fname LIKE "%da%";
 
 -- Ending with da
 SELECT title, author_fname FROM books WHERE author_fname LIKE "%da";
 
 -- Starting with da
 SELECT title, author_fname FROM books WHERE author_fname LIKE "da%";
+
+-- where stock quantity is 4 characters long
+SELECT title, stock_quantity FROM books WHERE stock_quantity LIKE "__";
+
+--  search for book title with '%' character
+SELECT title FROM books WHERE title LIKE "%\%%";
+
+--  search for book title with '_' character
+SELECT title FROM books WHERE title LIKE "%\_%";
