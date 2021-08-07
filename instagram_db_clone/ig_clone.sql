cfe4c80fc40081bf1c850bb1e72137dcb45af2e2
arryan <arryan@umich.edu>
2021-08-07 14:52:11 -0400

    add photos schema

--- instagram_db_clone/ig_clone.sql
+++ instagram_db_clone/ig_clone.sql
@@ -1,3 +1,4 @@
+DROP DATABASE ig_clone;
 CREATE DATABASE ig_clone;
 USE ig_clone;
 
@@ -5,4 +6,12 @@ CREATE TABLE users (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) UNIQUE NOT NULL,
   created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photos (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  image_url VARCHAR(255) not null,
+  user_id INTEGER NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY(user_id) REFERENCES users(id)
 );
